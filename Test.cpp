ede8332adbbb2c981b7be0e880986b41c19c21cf
DavidHarlap <adavid12@gmail.com>
2021-05-05 12:42:04 +0300

    add all HPP and CPP

---
+++ Test.cpp
@@ -0,0 +1,68 @@
+#include "doctest.h"
+
+#include "Board.hpp"
+#include "City.hpp"
+#include "Color.hpp"
+#include "Player.hpp"
+
+#include "Researcher.hpp"
+#include "Scientist.hpp"
+#include "FieldDoctor.hpp"
+#include "GeneSplicer.hpp"
+#include "OperationsExpert.hpp"
+#include "Dispatcher.hpp"
+#include "Medic.hpp"
+#include "Virologist.hpp"
+
+#include <iostream>
+#include <stdexcept>
+
+TEST_CASE("Initialize a board ")
+{
+
+    
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+    CHECK_EQ(1,1);
+}
+    
